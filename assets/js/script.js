6e056e2194061920c4920f043724ea8c84f87a35
deepak2509 <155398117+deepak2509@users.noreply.github.com>
2024-12-09 17:38:41 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Science, Engineering,Analytics"],
+    strings: ["Machine Learning, Data Professional"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
